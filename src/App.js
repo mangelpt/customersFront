acb0e6461b75888ab33c13f1f7805dd84c4b757e
miguel <mg20101994@gmail.com>
2022-01-24 00:39:01 -0500

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import {Routes, Route} from "react-router-dom";
 import PageLayout from "./pages/LayoutPage";
-import Dashboard from "./pages/Dashboard";
-import CustomerDetail from "./pages/CustomerDetail";
-import NewCustomer from "./pages/NewCustomer";
-import Average from "./pages/Average";
+import React, {Suspense} from "react";
+import {Spinner} from "./components/UI/Spinner";
+
+const Dashboard = React.lazy(() => import('./pages/Dashboard'))
+const CustomerDetail = React.lazy(() => import('./pages/CustomerDetail'))
+const NewCustomer = React.lazy(() => import('./pages/NewCustomer'))
+const Average = React.lazy(() => import('./pages/Average'))
 
 function App() {
     return (
-        <Routes>
-            <Route element={<PageLayout/>}>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/customers" element={<Dashboard/>}/>
-                <Route path="customers/:customerId" element={<CustomerDetail/>}/>
-                <Route path="/new" element={<NewCustomer/>}/>
-                <Route path="/average" element={<Average/>}/>
-            </Route>
-        </Routes>
+        <Suspense fallback={<Spinner/>}>
+            <Routes>
+                <Route element={<PageLayout/>}>
+
+                    <Route path="/" element={<Dashboard/>}/>
+                    <Route path="/customers" element={<Dashboard/>}/>
+                    <Route path="customers/:customerId" element={<CustomerDetail/>}/>
+                    <Route path="/new" element={<NewCustomer/>}/>
+                    <Route path="/average" element={<Average/>}/>
+                </Route>
+            </Routes>
+        </Suspense>
     );
 }
 
